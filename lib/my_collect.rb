64de98248706579ac829d805714a305b5fc9287b
Cassandra <cparisi1290@gmail.com>
2020-12-11 10:37:03 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(collection)
+    i = 0
+    new_collection = []
+    while i < collection.length
+        new_collection << yield(collection[i])
+        i += 1
+    end
+    new_collection
+end
 
 
+my_collect(collection) {|language| language.inspect.upcase}
